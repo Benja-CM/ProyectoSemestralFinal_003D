b5d5eb3aa8f8230a9fc007d9752300ae3e63bc46
3xc31 <al.silvab@duocuc.cl>
2023-05-18 12:03:06 -0400

    v0.075

--- GreenOasis/core/models.py
+++ GreenOasis/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class X(models.Model):
+    codid = models.AutoField(primary_key=True, verbose_name='Codigo ID x')
+    nombrex = models.CharField(max_length=20, blank=False, null=False)
+
+class X2(models.Model):
+    codx = models.CharField(max_length=28)
+    nombrex = models.CharField(max_length=30)
+    edadx = models.IntegerField()
+    foto = models.ImageField(upload_to="x2")
+    x = models.ForeignKey(X, on_delete=models.CASCADE)
