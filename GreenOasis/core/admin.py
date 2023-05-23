b9676af55285a2ea9cd88aaa7a4366cd7e3f377e
3xc31 <al.silvab@duocuc.cl>
2023-05-23 11:54:58 -0400

    v0.08

--- GreenOasis/core/admin.py
+++ GreenOasis/core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Rol, Usuario, Categoria, Producto, Region, Comuna, direccion, Estado, Compra, Detalle
 # Register your models here.
+
+admin.site.register(Rol)
+admin.site.register(Usuario)
+admin.site.register(Categoria)
+admin.site.register(Producto)
+admin.site.register(Region)
+admin.site.register(Comuna)
+admin.site.register(direccion) #Poner d mayus
+admin.site.register(Estado)
+admin.site.register(Compra)
+admin.site.register(Detalle)
--- GreenOasis/core/models.py
+++ GreenOasis/core/models.py
@@ -3,44 +3,56 @@ from django.db import models
 # Create your models here.
 class Rol(models.Model):
     id_rol      = models.AutoField(primary_key=True, verbose_name='ID de Rol')
-    rol_nom     = models.CharField(blank=False, verbose_name='Nombre de Rol')
+    rol_nom     = models.CharField(max_length=20, blank=False, verbose_name='Nombre de Rol')
+    def __str__(self) -> str:
+        return self.rol_nom
 
 class Usuario(models.Model):
     id_usuario  = models.AutoField(primary_key=True, verbose_name='ID de Usuario')
-    us_rut      = models.CharField(blank=False, verbose_name='Rut de Usuario')
-    us_nombre   = models.CharField(blank=False, verbose_name='Nombre de Usuario')
-    us_apellido = models.CharField(blank=False, verbose_name='Apellido de Usuario')
-    us_telefono = models.CharField(blank=False, verbose_name='Telefono de Usuario')
+    us_rut      = models.CharField(max_length=20, blank=False, verbose_name='Rut de Usuario')
+    us_nombre   = models.CharField(max_length=20, blank=False, verbose_name='Nombre de Usuario')
+    us_apellido = models.CharField(max_length=20, blank=False, verbose_name='Apellido de Usuario')
+    us_telefono = models.CharField(max_length=20, blank=False, verbose_name='Telefono de Usuario')
     rol         = models.ForeignKey(Rol, on_delete=models.CASCADE)
-    us_correo   = models.CharField(blank=False, verbose_name='Correo de Usuario')
-    us_clave    = models.CharField(blank=False, verbose_name='Clave de Usuario')
+    us_correo   = models.CharField(max_length=20, blank=False, verbose_name='Correo de Usuario')
+    us_clave    = models.CharField(max_length=20, blank=False, verbose_name='Clave de Usuario')
+    def __str__(self) -> str:
+        return self.us_nombre
 
 class Categoria(models.Model):
     id_cat      = models.AutoField(primary_key=True, verbose_name='ID de Categoria')
-    cat_nom     = models.CharField(blank=False, verbose_name='Nombre de Categoria')
+    cat_nom     = models.CharField(max_length=20, blank=False, verbose_name='Nombre de Categoria')
+    def __str__(self) -> str:
+        return self.cat_nom
 
 class Producto(models.Model):
     id_prod     = models.AutoField(primary_key=True, verbose_name='ID de Producto')
-    prod_nom    = models.CharField(blank=False, verbose_name='Nombre de Producto')
+    prod_nom    = models.CharField(max_length=20, blank=False, verbose_name='Nombre de Producto')
     prod_descripción = models.TextField(blank=False)
     prod_precio = models.IntegerField(null=False, blank=False)
     prod_stock  = models.IntegerField(null=False, blank=False)
     prod_imagen = models.ImageField(upload_to="Productos")
     categoria   = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return self.prod_nom
 
 class Region(models.Model):
     id_region   = models.AutoField(primary_key=True, verbose_name='ID de Region')
-    reg_nom     = models.CharField(blank=False, verbose_name='Nombre de Region')
+    reg_nom     = models.CharField(max_length=20, blank=False, verbose_name='Nombre de Region')
+    def __str__(self) -> str:
+        return self.reg_nom
 
 class Comuna(models.Model):
     id_region   = models.AutoField(primary_key=True, verbose_name='ID de Comuna')
-    com_nom     = models.CharField(blank=False, verbose_name='Nombre de Comuna')
-    com_cost_envio = models.IntegerField(null=False, blank=False) #Pertenece a la tabla como supertipo
+    com_nom     = models.CharField(max_length=20, blank=False, verbose_name='Nombre de Comuna')
+    com_cost_envio = models.IntegerField(null=False, blank=False) #Debe venir de la otra tabla
     region      = models.ForeignKey(Region, on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return self.com_nom
 
-class direccion(models.Model):
+class direccion(models.Model): #Cambiar d con Mayus
     id_dir      = models.AutoField(primary_key=True, verbose_name='ID de Dirección')
-    dir_calle   = models.CharField(blank=False, verbose_name='Nombre de Calle')
+    dir_calle   = models.CharField(max_length=20, blank=False, verbose_name='Nombre de Calle')
     dir_numero  = models.IntegerField(null=False, blank=False)
     dir_cod_postal = models.IntegerField(null=False, blank=False)
     usuario      = models.ForeignKey(Usuario, on_delete=models.CASCADE)
@@ -48,7 +60,7 @@ class direccion(models.Model):
 
 class Estado(models.Model):
     id_estado   = models.AutoField(primary_key=True, verbose_name='ID de Estado')
-    est_nombre  = models.CharField(blank=False, verbose_name='Nombre de Estado')
+    est_nombre  = models.CharField(max_length=20, blank=False, verbose_name='Nombre de Estado')
 
 class Compra(models.Model):
     id_compra   = models.AutoField(primary_key=True, verbose_name='ID de Compra')
