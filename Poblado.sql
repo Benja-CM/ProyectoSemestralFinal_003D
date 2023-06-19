e356a0ec2d71e949f614efb47e8d6da659caa323
Benja-CM <benj.contrerasm@duocuc.cl>
2023-06-18 20:05:32 -0400

    Update Poblado.sql

--- Poblado.sql
+++ Poblado.sql
@@ -1,11 +1,68 @@
 INSERT INTO core_rol VALUES (1,'Cliente');
 INSERT INTO core_rol VALUES (2,'Vendedor');
 
+------ CATEGORIAS -----------
 INSERT INTO core_categoria VALUES (1, 'Herramientas');
 INSERT INTO core_categoria VALUES (2, 'Tierra');
 INSERT INTO core_categoria VALUES (3, 'Maceteros');
 INSERT INTO core_categoria VALUES (4, 'Semillas');
 
+------ PRODUCTOS ------------
+INSERT INTO core_producto VALUES (1, 'Tijeras de Podar',
+'Las tijeras de podar son herramientas imprescindibles para cualquier jardinero o entusiasta del cuidado de plantas y árboles. Estas tijeras están diseñadas para cortar ramas y tallos de plantas de manera precisa y limpia, lo que ayuda a mantener una apariencia saludable y estética en los jardines y huertos.
+Nuestras tijeras de podar están fabricadas con materiales de alta calidad, lo que garantiza su durabilidad y resistencia. La cuchilla está hecha de acero inoxidable, lo que las hace resistentes a la oxidación y fácil de limpiar. El mango está hecho de materiales antideslizantes que proporcionan un agarre cómodo y seguro, incluso cuando las manos están húmedas o mojadas. Estas tijeras de podar son ideales para cortar ramas y tallos de hasta 2,5 centímetros de diámetro, y su diseño compacto y ligero las hace fáciles de usar incluso en lugares de difícil acceso. Además, su tamaño las hace ideales para llevarlas contigo mientras viajas o vas de camping. 
+Si estás buscando unas tijeras de podar de calidad y duraderas, no busques más. Nuestras tijeras de podar son la herramienta perfecta para cualquier tarea de poda en tu jardín o huerto.',
+9990, 24, 'Productos/prod1.jpg', 1);
+
+INSERT INTO core_producto VALUES (2, 'Pala Manual FiberComp® 100S',
+'La Pala Manual Puntiaguda FiberComp® 100S de la marca Fiskars es una herramienta de jardinería y construcción diseñada para cavar hoyos, trasplantar plantas y remover tierra en jardines y huertos. Con una longitud total de aproximadamente 100 cm y un peso de alrededor de 1 kg, esta pala es ligera y fácil de transportar y manejar.
+Está fabricada con la tecnología de material compuesto llamada FiberComp®, lo que la hace más resistente y duradera que las palas de acero tradicionales. Además, su mango está diseñado ergonómicamente para proporcionar una mayor comodidad y control durante su uso. Con su cabeza puntiaguda, la Pala Manual Puntiaguda FiberComp® 100S es ideal para cavar en suelos duros y compactos, lo que la hace adecuada tanto para proyectos de jardinería como de construcción pequeños.',
+6990, 15, 'Productos/prod2.jpg', 1);
+
+INSERT INTO core_producto VALUES (3, 'Pistola Riego Plástica con Gatillo Trasero 100S',
+'La Pistola Riego Plástica con Gatillo Trasero es una herramienta de riego especialmente diseñada para jardines y huertos. Está fabricada con plástico resistente y cuenta con un gatillo trasero que permite un mayor control del flujo de agua durante su uso.
+Además, esta pistola de riego cuenta con una boquilla ajustable que permite cambiar la configuración del chorro de agua según las necesidades del usuario. Es posible ajustar el chorro de agua desde un rocío fino hasta un chorro fuerte.
+El diseño ergonómico de la Pistola Riego Plástica con Gatillo Trasero proporciona un mango cómodo y antideslizante, lo que permite su uso prolongado sin experimentar fatiga en las manos.
+Esta herramienta de riego es fácil de usar gracias a su diseño sencillo e intuitivo, lo que hace que sea una excelente opción para los jardineros principiantes y experimentados. Además, es compatible con la mayoría de los sistemas de riego y mangueras de jardín.',
+8990, 25, 'Productos/prod3.jpg', 1);
+
+INSERT INTO core_producto VALUES (4, 'Aspersor de Riego Rectangular Regulable Plástico Gilmour',
+'El aspersor de riego rectangular regulable Gilmour es un dispositivo diseñado para regar áreas verdes de forma eficiente y efectiva. Este aspersor está fabricado en plástico resistente de alta calidad y cuenta con un diseño regulable que le permite adaptarse a diferentes necesidades de riego.
+Con una capacidad de riego de hasta 3,500 pies cuadrados (325 m²), este aspersor es ideal para regar grandes áreas de césped, jardines o huertos. Además, su patrón de rociado permite ajustar la dirección y el alcance del chorro de agua para cubrir áreas específicas, lo que lo hace especialmente útil para jardines y cultivos que requieren un riego más preciso.
+El aspersor de riego rectangular regulable Gilmour es resistente al desgaste y duradero en el tiempo, lo que garantiza un rendimiento de alta calidad durante muchos años. Su diseño intuitivo hace que sea fácil de configurar y utilizar para el riego de tu jardín o césped. Además, es compatible con la mayoría de los sistemas de riego y mangueras de jardín convencionales.',
+20480, 65, 'Productos/prod4.jpg', 1);
+
+INSERT INTO core_producto VALUES (5, 'Macetero Biodegradable Mediano Rojo',
+'El macetero biodegradable mediano rojo es una opción ecológica y práctica para cultivar plantas en el hogar o jardín. Está hecho de materiales biodegradables y compostables, por lo que no daña el medio ambiente después de su uso.
+El macetero tiene un tamaño mediano que lo hace adecuado para una variedad de plantas, y su color rojo brillante lo hace atractivo visualmente. Es resistente al agua y a los rayos UV, lo que significa que puede soportar diferentes condiciones climáticas.
+Además, al ser biodegradable, el macetero se puede plantar directamente en la tierra junto con la planta, lo que permite que las raíces penetren fácilmente y se integren con el suelo. Esto también ayuda a reducir el impacto ambiental de los residuos plásticos en los vertederos.',
+8000, 12, 'Productos/prod7.jpg', 3);
+
+INSERT INTO core_producto VALUES (6, 'Macetero Verde de Ceramica',
+'El macetero cerámico verde que ofrecemos está fabricado con cerámica de alta densidad y resistencia, lo que garantiza una estructura sólida y una durabilidad prolongada. Su superficie es lisa y uniforme gracias a su esmaltado y horneado a altas temperaturas, lo que le otorga una resistencia excepcional a las abrasiones y manchas. Además, su diseño minimalista y moderno lo convierte en una opción perfecta para cualquier decoración de interior. El tamaño del macetero está optimizado para una amplia gama de plantas en macetas y se adapta a la mayoría de los tamaños y formas de macetas estándar.',
+9000, 14, 'Productos/prod8.jpg', 3);
+
+INSERT INTO core_producto VALUES (7, 'Saco Tierra de Hojas 40 Litros',
+'Armony crea esta Tierra de hojas para tu jardín, compostando hojas y residuos vegetales provenientes de podas, entregandote un producto 100% natural y comprometido con el medio ambiente. Es un producto versátil que puede utilizarse tanto en el jardín como maceteros y almácigos. Esta tierra mantendrá tus espacios libres de malezas y enfermedades, evitándose costos extras en productos de cuidado. Además se encuentra desinfectada, harneada y presenta buen drenaje, por lo que mantendrá tus plantas y flores perfectamente hidratadas.',
+9500, 27, 'Productos/prod10.jpg', 2);
+
+INSERT INTO core_producto VALUES (8, 'Tierra de hoja 6 Litros',
+'Sustrato natural desarrollado a partir de la compostación de hojas y residuos vegetales, que mejora las condiciones de los suelos, tanto para el desarrollo de prados como el de otras plantas. Favorece la germinación y crecimiento del césped. Idealmente para ser usada en siembras y resiembra de prados en parques, plazas y jardines.',
+2490, 16, 'Productos/prod11.jpg', 2);
+
+INSERT INTO core_producto VALUES (9, 'Semilla PAQUERETTE Blanco GRANDE 0,4 GR',
+'Planta perenne que florece al año siguiente de su siembra. De grandes flores blancas de vara alta e ideal para usar en bordes de jardines y macizos florales.',
+2000, 25, 'Productos/prod13.jpg', 4);
+
+INSERT INTO core_producto VALUES (10, 'Semillas de Nabo Blanco',
+'La bolsa de semillas de nabos blancos de Las Encinas es una excelente opción para aquellos que desean cultivar sus propias verduras en casa. Cada bolsa contiene semillas de alta calidad que han sido cuidadosamente seleccionadas y procesadas para asegurar una germinación rápida y una producción abundante. Con un peso de sólo 0.001 kg y dimensiones de 10 × 13 × 10 cm, esta bolsa de semillas es fácil de almacenar y transportar. Son fáciles de cultivar y son una excelente opción para añadir a ensaladas y otros platos.',
+1990, 35, 'Productos/prod14.jpg', 4);
+-------------------------------------
+
+-------- USUARIO VENDEDOR -----------
+INSERT INTO core_usuario (ID_USUARIO, C_ALIAS, C_CORREO, C_PASSWORD, ROL_ID) VALUES (1, 'Fernando', 'fer.alvarezd@gmail.com', 'Asd1234#', 2);
+
+------------ COMUNAS ----------------
 INSERT INTO core_comuna VALUES (1, 'Cerrillos', 2000);
 INSERT INTO core_comuna VALUES (2, 'Cerro Navia', 1500);
 INSERT INTO core_comuna VALUES (3, 'Conchalí', 1000);
